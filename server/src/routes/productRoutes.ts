44a091c1f4544578fd7ba09235605f5da9ec21d7
mtthdz <matt@mtthdz.com>
2023-11-07 22:35:56 -0500

    (add) split up productRoutes to include search endpoint

--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -6,9 +6,32 @@ const productRoutes = express.Router();
 
 productRoutes.route('/').get( async (req: Request, res: Response) => {
   try {
-    let productList: ProductType[] = productData;
-    res.json(productList);
+    res.json(productData as ProductType[]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
 
+/**
+ * Search Endpoint
+ * 
+ * Check for search term
+ * if no search term, return 400 error
+ */
+productRoutes.route('/search').get( async ( req: Request, res: Response) => {
+  try {
+    const { filterBy } = req.query;
+
+    if (!filterBy || typeof filterBy !== 'string') {
+      return res.status(400).json({ error: 'Invalid or missing search term' });
+    }
+
+    const filteredProducts = productData.filter((product) =>
+      product.product.toLowerCase().includes(filterBy.toLowerCase())
+    );
+
+    res.json(filteredProducts as ProductType[]);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
