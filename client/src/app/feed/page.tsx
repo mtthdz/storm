21acec1d2f3db4e74236a27a3965dddc56bf074b
mtthdz <matt@mtthdz.com>
2023-11-11 22:24:45 -0500

    (add) context api for feed feature

--- client/src/app/feed/page.tsx
+++ client/src/app/feed/page.tsx
@@ -6,9 +6,28 @@ import ListingTable from "@/components/sections/ListingTable";
 import Modal from "@/components/sections/Modal";
 import { FeedContext } from "@/context/FeedContext";
 import { getProducts } from "@/api/getProducts";
+import { ProductType } from "@/types/ProductType";
 
+/**
+ * Feed page
+ * 
+ * Feed page displays all products.
+ * Because this code structure is feature-driven, this component
+ * acts as the parent-most component for all state, via context
+ * 
+ * Interviewer Notes
+ * I typically store global data (such as user) into a redux store,
+ * but have set a fixed instance of the user object here due to
+ * time constraints.
+ * 
+ * 
+ * @returns JSX
+ */
 export default function Feed() {
+  const [user, setUser] = useState('Adriana Arias')
   const [modalVisible, setModalVisible] = useState(false);
+  const [feedData, setFeedData] = useState<ProductType[]>([]);
+  const [selectedItem, setSelectedItem] = useState<ProductType>(null);
 
   useEffect(() => {
     getProducts();
@@ -17,8 +36,14 @@ export default function Feed() {
   return (
     <FeedContext.Provider
       value={{
+        user,
         modalVisible,
+        feedData,
+        selectedItem,
+        setUser,
         setModalVisible,
+        setFeedData,
+        setSelectedItem,
       }}
     >
       <>
--- client/src/context/FeedContext.ts
+++ client/src/context/FeedContext.ts
@@ -1,11 +1,24 @@
+import { ProductType } from "@/types/ProductType";
 import { Dispatch, SetStateAction, createContext } from "react";
 
 type FeedContextProps = {
+  user: null | string;
   modalVisible: null | boolean;
+  feedData: null | ProductType[];
+  selectedItem: null | ProductType;
+  setUser: Dispatch<SetStateAction<string>>;
   setModalVisible: Dispatch<SetStateAction<boolean>>;
+  setFeedData: Dispatch<SetStateAction<ProductType[]>>;
+  setSelectedItem: Dispatch<SetStateAction<ProductType>>;
 };
 
 export const FeedContext = createContext<FeedContextProps>({
+  user: 'Adriana Arias',
   modalVisible: false,
+  feedData: [],
+  selectedItem: null,
+  setUser: () => {},
   setModalVisible: () => {},
+  setFeedData: () => {},
+  setSelectedItem: () => {},
 });
